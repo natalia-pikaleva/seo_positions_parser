fb8ab4d8538d01f843ab1098acf99d1833f55260
root <root@leddmxtemc.local>
2026-01-15 13:46:41 +0000

    saving the changes

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,4 +1,4 @@
-//export const HOST = 'https://parser.re-spond.com'; // замените на ваш адрес бэкенда
-//export const API_BASE = 'https://parser.re-spond.com/api'; // замените на ваш адрес бэкенда
-export const API_BASE = 'http://localhost:8000/api';
-export const HOST = 'http://localhost:8000'; // замените на ваш адрес бэкенда
+export const HOST = 'https://parser.re-spond.com'; // замените на ваш адрес бэкенда
+export const API_BASE = 'https://parser.re-spond.com/api'; // замените на ваш адрес бэкенда
+//export const API_BASE = 'http://localhost:8000/api';
+//export const HOST = 'http://localhost:8000'; // замените на ваш адрес бэкенда
